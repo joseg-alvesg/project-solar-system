e789a1655e74ba5743033e758b9fb3f8b62b530c
joseg-alvesg <cinhoalves85@gmail.com>
2022-11-25 18:13:07 -0300

    Requisito 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
-    const { headline } = this.props;
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        { headline }
+        { missions.map(({ name, year, country, destination }) => (
+          <MissionCard
+            key={ name }
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+          />
+        ))}
       </div>
     );
   }
 }
-
-Missions.propTypes = {
-  headline: PropTypes.string.isRequired,
-};
 
 export default Missions;
