0cd8a362ad385377ba513bec75b91d7469089507
joseg-alvesg <cinhoalves85@gmail.com>
2022-11-25 17:21:44 -0300

    Requisito 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
+    const { headline } = this.props;
     return (
-      <div data-testid="missions" >
-
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        { headline }
       </div>
     );
   }
 }
+
+Missions.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Missions;
